8905935b99fef309d7f9a78143a0974172e6cd66
Yiğit Dur <ygtdr35@gmail.com>
2023-05-12 23:10:39 +0300

    .

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 from flask import Flask, render_template, request, redirect
 import pandas as pd
 import os
+import tkinter as tk
+import webbrowser
+import openpyxl
+import threading
 
-app = Flask(__name__)
 
-if not os.path.isfile("demo.xlsx"):
-    writer = pd.ExcelWriter("demo.xlsx", engine="xlsxwriter")
-    df = pd.DataFrame({
-        'Name': [],
-        'Last Name': [],
-        'Car Model': [],
-        'Car Km': [],
-        'Car Year': [],
-        'Phone': []
-    })
-    df.to_excel(writer, index=False)
-    writer.close()
+def run_flask_app():
+    app.run()
+
+
+def open_flask_webpage():
+    url = 'http://localhost:5000'
+    webbrowser.open_new(url)
+
+
+def open_webpage():
+    thread = threading.Thread(target=run_flask_app)
+    thread.start()
+
+
+app = Flask(__name__, static_folder="static", template_folder="templates")
+
 
 @app.route("/api/car/save", methods=["POST", "GET"])
 def save_car():
@@ -26,8 +33,7 @@ def save_car():
     car_year = request.form.get("cyear")
     phone_number = request.form.get("phonnum")
 
-
-    reader = pd.read_excel('demo.xlsx', engine="openpyxl")
+    reader = pd.read_excel('dataexcel.xlsx', engine="openpyxl")
     df = pd.DataFrame({
         'Name': [first_name],
         'Last Name': [last_name],
@@ -35,7 +41,7 @@ def save_car():
         'Car Km': [car_km],
         'Car Year': [car_year],
         'Phone': [phone_number]})
-    writer = pd.ExcelWriter("demo.xlsx", engine="openpyxl", mode="a", if_sheet_exists="overlay")
+    writer = pd.ExcelWriter("dataexcel.xlsx", engine="openpyxl", mode="a", if_sheet_exists="overlay")
     df.to_excel(writer, sheet_name='Sheet1', index=False, header=False, startrow=len(reader) + 1)
     writer.close()
 
@@ -48,4 +54,22 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run()
+    if not os.path.isfile("dataexcel.xlsx"):
+        writer = pd.ExcelWriter("dataexcel.xlsx", engine="openpyxl")
+        df = pd.DataFrame({
+            'Name': [],
+            'Last Name': [],
+            'Car Model': [],
+            'Car Km': [],
+            'Car Year': [],
+            'Phone': []
+        })
+        df.to_excel(writer, index=False)
+        writer.close()
+
+    threading.Thread(target=open_webpage).start()
+
+    window = tk.Tk()
+    button = tk.Button(window, text='Web Sayfasını Aç', command=open_flask_webpage)
+    button.pack()
+    window.mainloop()
